40e70ba81ba22901d092bea8f202ff85525e7ca5
pawan <pawnu@users.noreply.github.com>
2020-05-21 17:32:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,11 @@ sudo apt-get install -y docker-compose
 
 #let non root user user docker
 sudo usermod -aG docker $(whoami)
-sudo setfacl -m user:$(whoami):rw /var/run/docker.sock
 
 #change owner of godata folder to that of gocd server user which has uid 1000
 sudo chown -R 1000 godata
 
 #build the gocd server and agent container
 docker-compose up -d --build
+
+sudo setfacl -m user:$(whoami):rw /var/run/docker.sock
