5fb141a23ca8370442655961e7cadf8d4a2abde8
pawan <pawnu@users.noreply.github.com>
2020-05-21 16:28:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt-get install acl -y
 sudo apt install docker.io -y
 sudo apt-get install -y docker-compose
 
 #let non root user user docker
 sudo usermod -aG docker $(whoami)
+sudo setfacl -m user:$(whoami):rw /var/run/docker.sock
 
 #change owner of godata folder to that of gocd server user which has uid 1000
 sudo chown -R 1000 godata
